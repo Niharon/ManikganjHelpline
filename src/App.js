5403aea3473b2b271b43ec9596aa8bd9a8e53018
Sakhawat Hossain <96255034+Sakhawatdev@users.noreply.github.com>
2023-01-26 01:00:17 +0600

    first comment

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MainPage from "./components/MainPage";
+
+function App() {
+  return (
+    <div>
+      <MainPage />
+      {/* <h1> App js page</h1> */}
+    </div>
+  );
+}
+// 39 minutes 22 seconds
+export default App;
+
+// home/hospital -> hospital page
+// home/clinic -> clinic page
+// home/ambulance ->ambulance page
+
+// home/hospital -> Detail (hospital)
+// home/clinic -> Detail (clinic)
+// home/ambulance -> Detail (ambulance)
